040551d628948f7e2b011b1b7c7b545068dfaf90
LidiaEvmenenko <inu-chan@mail.ru>
2021-08-08 16:31:00 +0500

    Добавили картинку фона, закомментили лишний код, отрисовали фон

--- core/src/ru/gb/StarGame.java
+++ core/src/ru/gb/StarGame.java
@@ -11,26 +11,25 @@ public class StarGame extends ApplicationAdapter {
 	Texture img;
 	TextureRegion region;
 
-
 	private int x = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 25, 25, 100, 50);
+		img = new Texture("fon.jpg");
+		//region = new TextureRegion(img, 25, 25, 100, 50);
 	}
 
 	@Override
 	public void render () {
 		//x++;
-		ScreenUtils.clear(0.4f, 0.24f, 0.51f, 1);
+		//ScreenUtils.clear(0.4f, 0.24f, 0.51f, 1);
 		batch.begin();
-		batch.draw(img, x, 0);
-		batch.setColor(0.67f, 0.33f, 0.12f, 0.5f);
-		batch.draw(img , 300, 300, 100, 100);
-		batch.setColor(0.01f, 0.01f, 0.90f, 1f);
-		batch.draw(region, 260, 150,100,50);
+		batch.draw(img, 0, 0);
+		//batch.setColor(0.67f, 0.33f, 0.12f, 0.5f);
+		//batch.draw(img , 300, 300, 100, 100);
+		//batch.setColor(0.01f, 0.01f, 0.90f, 1f);
+		//batch.draw(region, 260, 150,100,50);
 		batch.end();
 	}
 	
